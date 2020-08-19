af5f78265198245c862af4fbcaa78e57a00f2797
Rick Belson <belsonrj@aol.com>
2020-08-19 22:41:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -41,6 +41,8 @@ class Square extends Polygon {
   }
   
   get area() {
-    return this.sideLength * this.sideLength;
+    if ( !Array.isArray( this.sides ) ) return;
+    if ( this.count !== 4 ) return;
+    return this.sides[ 0 ] * this.sides[ 0 ]
   }
 }
