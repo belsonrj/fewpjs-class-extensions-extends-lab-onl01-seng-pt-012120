0da677a482e5b3f29a94a0c2fe8368472572bbdc
Rick Belson <belsonrj@aol.com>
2020-08-19 22:41:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,9 +29,17 @@ class Triangle extends Polygon {
 }
 
 class Square extends Polygon {
+  
   get isValid() {
-    
+    if ( !Array.isArray( this.sides ) ) return;
+    if ( this.count !== 4 ) return;
+    let side1 = this.sides[ 0 ]
+    let side2 = this.sides[ 1 ]
+    let side3 = this.sides[ 2 ]
+    let side4 = this.sides[ 3 ]
+    return ( ( side1 === side2 ) && ( side1 === side3 ) && ( side1 === side4 ) )
   }
+  
   get area() {
     return this.sideLength * this.sideLength;
   }
